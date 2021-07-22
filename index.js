be09af0bfb08b91b562ba63f41e4ade7b4139392
Hieu <xuanhieu0012@gmail.com>
2021-07-22 17:04:27 -0500

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your code here
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1>Hieu Nguyen is the champion</h1>";
